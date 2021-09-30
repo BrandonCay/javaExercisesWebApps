17a305469d458707c997a5562172fe85ddb03cf8
BrandonCay <brandoncay525@gmail.com>
2021-09-29 20:54:32 -0400

    all

---
+++ code/Hands-On-P1/Program1.java
@@ -0,0 +1,7 @@
+/*--------------------------------------Program1 -------------------------------------*/
+public class Program1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Java, here we come");
+    }
+}
---
+++ code/Hands-On-P1/Program2.java
@@ -0,0 +1,24 @@
+
+/*----------------------------------Program2 --------------------------------------------*/
+/* Read two integer values, compute their product */
+import java.util.Scanner;
+
+public class Program2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); // for standard input
+        int first, // to hold the first value
+                second; // to hold the second
+        /*--------------------read the two values -------------------------------*/
+        first = input.nextInt();
+        second = input.nextInt();
+        /*-----------------Compute and print their product--------------------*/
+        int product = computeProd1(first, second);
+        System.out.println("Their product is:\t" + product);
+    }
+
+    /*------------------------Class Method computeProd( )-------------------------------*/
+    /* compute the product of two integer values and returns it */
+    static int computeProd1(int num1, int num2) {
+        return (num1 * num2);
+    }
+}
