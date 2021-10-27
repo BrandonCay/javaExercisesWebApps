5bd10bd2a72afa47451d939af0f598df971ad6f1
BrandonCay <brandoncay525@gmail.com>
2021-10-26 20:18:41 -0400

    exercise3 finished

--- code/ExerciseO3/ExerciseO3.java
+++ code/ExerciseO3/ExerciseO3.java
@@ -1,52 +1,34 @@
 import java.util.Scanner;
 
-import javax.lang.model.util.ElementScanner14;
-/*
-Define a class named ExerciseO3 with the method main that does the following:
-
-It first defines an object with the default date and then outputs this date as a string.
-
-It defines an object named today and initializes it with today’s date.
-
-It outputs the message “Today’s day is:\t” followed by today’s date as a string.
-
-It defines an object named dueDate and then reads values for its instance variables.
-
-It uses the instance method isEqualTo( ) to compare the object today and dueDay and if 
-they are equal, it outputs the message “Your project is on time”; 
-otherwise, it uses the method isGreaterThan(  ) 
-to compare these objects again and then output either the message 
-“Your project is late” or “Your project is early” as appropriate.
-
-It finally reads or sets an object with an invalid date (for example, 25, 4, 2000).
-
-*/
 public class ExerciseO3{
     
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
         Date obj = new Date();
-        obj.inputDate(scan);
-
-        System.out.println("Today's day is:\t" + obj.getStringDate());
+        
+        obj.inputDate(scan);//todays date is invalid
+        System.out.println("Today's the day " + obj.getStringDate());
     
         Date dueDate = new Date();
 
         dueDate.inputDate(scan);
+
 
         String message;
 
         if(obj.isEqualTo(dueDate))
             message = "Your project is on time";
         else if(Date.isGreaterThan(obj, dueDate)){
-            message="Your project is on time";
+            message="Your project is late";
         }else
             message="Your project is early";
 
         System.out.print(message);
-        
+
+
         Date invalid = new Date();
         invalid.inputDate(scan);
+        System.out.println("Program successfully finished"); //doesn't print when invalid date is inputted
         
         
     }
