3c855c6c3c9a52271d9724ece16c5d42e2d54d6d
BrandonCay <brandoncay525@gmail.com>
2021-10-27 18:56:33 -0400

    finished E6

--- code/ExerciseO6/Score1.java
+++ code/ExerciseO6/Score1.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /*
 Write the definition of the class named Score1 with the following private instance variables:
@@ -38,6 +39,51 @@ that is passed to it as an argument.
         }
     }
 
-    
+/*
+In addition to the constructors, it has the following public instance methods:  
+*/
+
+/*
+double computeAverage(  ) that computes the average score,  
+*/
+
+public double computeAverage(){
+    double sum  = 0;
+    for(int i = 0 ; i < arrSz ; ++i){
+        sum+=testScores[i];
+    }
+    return sum / arrSz; 
+}
+
+/*
+void read(Scanner scan ) that uses the Scanner object parameter to read the first name, last name, course name,
+and the 5 test scores into the array, 
+*/
+
+public void read(Scanner scan){
+    firstName = scan.next();
+    lastName = scan.next();
+    courseName = scan.next();
+
+    for(int i = 0; i < arrSz ; ++i){
+        testScores[i] = scan.nextDouble(); 
+    } 
+
+}
+
+/*and
+void print( ) that outputs the first name, last name, course name, the test scores, and the average test score. 
+*/
+
+public void print(){
+    System.out.printf("%s\n%s\n%s\n",firstName, lastName, courseName);
+    for(double el : testScores){
+        System.out.println(el);
+    }
+
+    double avg = computeAverage();
+    System.out.print(avg +  "\n");
+}
+
 
 }
