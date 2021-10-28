ae4914e053abdc9a651f60758ad80e0ff494b5d9
BrandonCay <brandoncay525@gmail.com>
2021-10-27 20:50:35 -0400

    e9 fin

--- code/ExerciseO9/ExerciseO9.java
+++ code/ExerciseO9/ExerciseO9.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class ExerciseO9{
     public static void main(String [] main){
         Employee  eemp  = new Employee(   );
@@ -6,6 +7,26 @@ public class ExerciseO9{
         
         BonusEmployee  bemp2  =  new  BonusEmployee(   );
         
+        Scanner scan  = new Scanner( System.in );
+
+        eemp.readPayInfo( scan  );
         
+        bemp1.readPayInfo(  scan );
+        
+        bemp2.readPayInfo( scan  );
+        
+        System.out.println( "Gross Pay of eemp=\t" +   (eemp.getGpay(  )) );
+        
+        System.out.println( "Gross Pay of bemp1=\t" +   (bemp1.getGpay(  )) );
+        
+        System.out.println( "Gross Pay of bemp2=\t" +   (bemp2.getGpay(  )) );
+        
+        System.out.println( "Tax of eemp=\t" +   (eemp.computeTax(  )) );
+        
+        System.out.println( "Tax of bemp1=\t" +   (bemp1. computeTax (  )) );
+        
+        System.out.println( "Tax of bemp2=\t" +   (bemp2. computeTax (  )) );
+
+        //double  bonus  =  bemp1.getBonus(   );  NO, IT IS UNDEFINED
     }
 }
