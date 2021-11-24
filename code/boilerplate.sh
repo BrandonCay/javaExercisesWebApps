5bf27af9053e1b3a70376ee896700b990c29961b
BrandonCay <brandoncay525@gmail.com>
2021-11-24 15:37:18 -0500

    P: boilerplate done TD: rest of ex

--- code/boilerplate.sh
+++ code/boilerplate.sh
@@ -6,8 +6,8 @@ inputFileName=p
 stdoutFileName=${inputFileName}
 stderrFileName=err
 runFileName=run
+defaultJavaFileName=main
 
-touch ${inputFileName}.dat ${stdoutFileName}.out ${stdoutFileName}.out main.java;
-cat "fileName=m;javac \${fileName}.java;java \${fileName} \ 
-< ${inputFileName}.dat 1> ${stdoutFileName}.out 2> ${stderrFileName}.out " > ${runFileName}.sh
+touch ${inputFileName}.dat ${stdoutFileName}.out ${stderrFileName}.out ${defaultJavaFileName}.java ${runFileName}.sh;
+printf "fileName=m;\njavac \${fileName}.java;\njava \${fileName} < ${inputFileName}.dat 1> ${stdoutFileName}.out 2> ${stderrFileName}.out" > ${runFileName}.sh;
 
