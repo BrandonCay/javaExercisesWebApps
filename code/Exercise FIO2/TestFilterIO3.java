20247ccecd573e3943e8c32e1730be085871e7d5
BrandonCay <brandoncay525@gmail.com>
2021-11-24 23:34:02 -0500

    fin ex td: fin last ex

--- code/Exercise FIO2/TestFilterIO3.java
+++ code/Exercise FIO2/TestFilterIO3.java
@@ -2,20 +2,26 @@ import java.io.*;
 
 public class TestFilterIO3 {
     public static void main(String args[]) {
-        int[] ilist = { 2, 13, 24, 35, 46 };
-        double[] dlist = { 3.5, 13.6, 24.7, 35.8, 46.9 };
-        String[] slist = { "John", "Mark", "Peter", "Ann", "Vicky" };
+        int inum = 7;
+        double dnum = 10.5;
+        String name = "John Doe";
+//faster than deleting loops. Less destructive route
+
+        int[] ilist = { inum };
+        double[] dlist = { dnum };
+        String[] slist = { name };
+        int arraySz = 1;
         try {
             /*--- Create a byte-based output stream for the file: fio3.data---*/
             FileOutputStream foutput = new FileOutputStream("fio3.data");
             /* connect a DataOutputStream filter to this stream */
             DataOutputStream doutput = new DataOutputStream(foutput);
             /* write the elements of the arrays into this file */
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < arraySz; i++)
                 doutput.writeInt(ilist[i]);
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < arraySz; i++)
                 doutput.writeDouble(dlist[i]);
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < arraySz; i++)
                 doutput.writeUTF(slist[i]);
             doutput.close();
             foutput.close();
@@ -29,15 +35,15 @@ public class TestFilterIO3 {
             PrintStream poutput = new PrintStream(newfoutput);
             /*------read the content of the binary file fio3.data and
              write it into the text file fio3.txt -----------*/
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < arraySz; i++) {
                 int inun = dinput.readInt();
                 poutput.println(inun);
             }
-            for (int i = 0; i < 5; i++) {
-                double dnum = dinput.readDouble();
-                poutput.println(dnum);
+            for (int i = 0; i < arraySz; i++) {
+                double dnum2 = dinput.readDouble();
+                poutput.println(dnum2);
             }
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < arraySz; i++) {
                 String st = dinput.readUTF();
                 poutput.println(st);
             }
